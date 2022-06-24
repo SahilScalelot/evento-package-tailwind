23ab6667b8fa7a72b9f27e5680e810dc2ef5e693
SahilScalelot <devsahil.scalelot@gmail.com>
2022-06-24 15:52:04 +0530

    New Pages Add

--- public/assest/js/script.js
+++ public/assest/js/script.js
@@ -22,3 +22,44 @@ $(document).ready(function () {
     $("#" + tab_id).addClass('active');
   });
 });
+
+var $titleTab = $('.title_tab');
+$('.title_tab.active').next('.inner_content').slideDown();
+$titleTab.on('click', function(e) {  
+e.preventDefault();  
+  if ( $(this).hasClass('active') ) {
+    $(this).removeClass('active');
+    $(this).next().stop().slideUp(500);
+    $(this).next().find('p').removeClass('show');
+  } else {
+    $(this).addClass('active');
+    $(this).next().stop().slideDown(500);
+    $(this).parent().siblings().children('.title_tab').removeClass('active');
+    $(this).parent().siblings().children('.inner_content').slideUp(500);
+    $(this).parent().siblings().children('.inner_content').find('p').removeClass('show');
+    $(this).next().find('p').addClass('show');
+  }
+});
+
+// gallery slider 
+
+var galleryTop = new Swiper('.gallery-top', {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+   // loop: true,
+  loopedSlides: 4
+});
+var galleryThumbs = new Swiper('.gallery-thumbs', {
+  spaceBetween: 10,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  touchRatio: 0.2,
+  slideToClickedSlide: true,
+  // loop: true,
+  loopedSlides: 4
+});
+galleryTop.controller.control = galleryThumbs;
+galleryThumbs.controller.control = galleryTop;
